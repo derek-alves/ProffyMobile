057d8f23dccef3388c9c29b2cbea8040a7a645b7
Derek Enrique Alves <59737946+derek-alves@users.noreply.github.com>
2020-08-19 17:17:43 -0300

    Pages: Favorite and Proffys

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, TextComponent } from 'react-native';
+import PageHeader from '../PageHeader';
 import styles from './styles';
 
 function Favorites(){
   return(
     <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos"/>
     </View>
   )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text, TextComponent } from 'react-native';
 import styles from './styles';
+import PageHeader from '../PageHeader';
 
 function TeacherList(){
   return(
     <View style={styles.container}>
-      <Text>TeacherList</Text>
+      <PageHeader title="Proffys disponíveis"/>
     </View>
   )
 }
